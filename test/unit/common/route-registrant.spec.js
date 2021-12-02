a5c497e82e96ff363ad27e50b944516a64316909
spomni <mikhail.snitko@gmail.com>
2021-12-02 13:38:34 +0300

    test: test route-registrant nodule

--- test/unit/common/route-registrant.spec.js
+++ test/unit/common/route-registrant.spec.js
@@ -11,18 +11,18 @@ jest.mock('fastify', () => {
   })
 })
 
-const Fastify = require('fastify')
+const app = require('fastify')()
 const RouteRegistrant = require(fromSrc('common/route-registrant'))
 const { create: createRegistrant } = RouteRegistrant
 
 describe('route-registrant', () => {
 
+  beforeEach(() => jest.clearAllMocks())
+
   describe('RouteRegistrant', () => {
 
     describe('.create()', () => {
       it('should return create a new RouterRegistrant instance', () => {
-        const app = Fastify()
-
         const registrant = createRegistrant(app)
 
         expect(registrant).toBeInstanceOf(RouteRegistrant)
@@ -30,10 +30,78 @@ describe('route-registrant', () => {
     })
 
     describe('#register()', () => {
-      it.todo('should add route by config');
-      it.todo('should add plugin by config');
-      it.todo('should works correct with configList');
-      it.todo('should throw an error if config is invalid');
+
+      it('should add route by config', () => {
+        const config = {
+          method: 'GET'
+        }
+
+        createRegistrant(app).register(config)
+
+        expect(app.route).toHaveBeenCalledWith(config)
+      });
+
+      it('should transform route method to upper case', () => {
+        const config = {
+          method: 'post'
+        }
+
+        createRegistrant(app).register(config)
+
+        expect(app.route).toHaveBeenCalledWith({
+          method: config.method.toUpperCase()
+        })
+      });
+
+      it('should add plugin by config', () => {
+        const plugin = jest.fn()
+        const options = {}
+        const config = { plugin, options }
+
+        createRegistrant(app).register(config)
+
+        expect(app.register).toHaveBeenCalledWith(plugin, options)
+      });
+
+      it('should works correct with configList', () => {
+
+        const configList = [
+          {
+            method: 'GET'
+          },
+          {
+            plugin: () => {},
+            options: {}
+          },
+          {
+            method: 'POST'
+          },
+          {
+            plugin: () => {},
+            options: {}
+          },
+        ]
+
+        createRegistrant(app).register(configList)
+
+        configList.forEach((config, index) => {
+          if (config.method) {
+            expect(app.route).toHaveBeenCalledWith(config)
+          }
+          if (config.plugin) {
+            const { plugin, options } = config
+            expect(app.register).toHaveBeenCalledWith(plugin, options)
+          }
+        })
+      });
+
+      it('should throw an error if config is invalid', () => {
+        const config = {}
+
+        const callRegister = () => createRegistrant(app).register(config)
+
+        expect(callRegister).toThrow(/invalid.+config/)
+      });
     });
   })
 })
