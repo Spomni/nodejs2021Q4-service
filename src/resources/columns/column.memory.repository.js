0ad22fc88f469fb46bea72228408f4306b8f5f13
spomni <mikhail.snitko@gmail.com>
2021-12-02 17:06:17 +0300

    refactor: use MemoryRepository to store columns

--- src/resources/columns/column.memory.repository.js
+++ src/resources/columns/column.memory.repository.js
@@ -1,28 +1,3 @@
-const { isArray } = Array
+const MemoryRepository = require('../../common/memory-repository')
 
-const collection = []
-
-function add(column) {
-  const list = (isArray(column)) ? column : [column]
-  collection.push(...list)
-}
-
-function getOnce(condition) {
-  return collection.find(condition)
-}
-
-function get(condition) {
-  return collection.filter((item) => condition(item))
-}
-
-function remove(condition) {
-  const index = collection.findIndex(condition)
-  collection.splice(index, 1)
-}
-
-module.exports = {
-  add,
-  getOnce,
-  get,
-  remove,
-}
+module.exports = MemoryRepository.create()
