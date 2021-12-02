1a77fb64433a747e8908043d9f103227d4b96778
spomni <mikhail.snitko@gmail.com>
2021-12-03 01:56:07 +0300

    chore: remove eslint disable line

--- src/common/route-registrant/route-registrant.js
+++ src/common/route-registrant/route-registrant.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line max-classes-per-file
 const { isArray } = Array
 const isRouteConfig = ({ method }) => Boolean(method)
 const isPluginConfig = ({ plugin }) => Boolean(plugin)
