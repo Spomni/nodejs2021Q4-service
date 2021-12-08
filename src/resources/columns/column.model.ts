a81bc80fdd8e1d0f5655bd5491460dcd35807808
spomni <mikhail.snitko@gmail.com>
2021-12-08 19:53:30 +0300

    docs: write TSDoc in the column.model module

--- src/resources/columns/column.model.ts
+++ src/resources/columns/column.model.ts
@@ -2,6 +2,9 @@ import { v4 as uuid } from 'uuid'
 import { IColumn } from "../../contract/resources/column.contract";
 import { IModel } from "../../contract/model.contract";
 
+/**
+ * Column model
+ */
 export class Column implements IColumn, IModel<IColumn> {
   id: string
 
@@ -19,29 +22,61 @@ export class Column implements IColumn, IModel<IColumn> {
     this.order = order
   }
 
-  toStorage() {
-    return Column.toStorage(this)
-  }
-
-  toResponse() {
-    return Column.toResponse(this)
-  }
-
-  static create(columnLike: IColumn) {
-    return new Column(columnLike)
-  }
-
+  /**
+   * Get a value o the "id" property from the Column instance
+   *
+   * @param column - column instance
+   * @returns - passed column id
+   */
   static getId(column: Column) {
     return column.id
   }
 
-  static toStorage(column: Column) {
+  /**
+   * Get a column that can be stored safely from this instance
+   *
+   * @returns - safely column
+   */
+  toStorage(): IColumn {
+    return Column.toStorage(this)
+  }
+
+  /**
+   * Get a column that can be sended safely from this instance
+   *
+   * @returns - safely column
+   */
+  toResponse(): IColumn {
+    return Column.toResponse(this)
+  }
+
+  /**
+   * Get a column that can be stored safely from passed instance
+   *
+   * @returns - safely column
+   */
+  static toStorage(column: Column): IColumn {
     const { id, title, order } = column
     return { id, title, order }
   }
 
-  static toResponse(column: Column) {
+  /**
+   * Get a column that can be sended safely from passed instance
+   *
+   * @returns - safely column
+   */
+  static toResponse(column: Column): IColumn {
     const { id, title, order } = column
     return { id, title, order }
+  }
+
+  /**
+   * Get a new instance f the Column class
+   *
+   * @param columnLike - initial column to create from
+   * @returns - new Column instance
+   */
+  static create(columnLike: IColumn) {
+    return new Column(columnLike)
   }
 }
