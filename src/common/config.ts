e4b1e9b3061d9f19dddac01ea6d7568dce542215
spomni <mikhail.snitko@gmail.com>
2021-12-09 12:01:34 +0300

    refactor: refactor common/config module with TS

---
+++ src/common/config.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env')
+});
+
+const {
+  PORT,
+  NODE_ENV,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+} = process.env
+
+const AUTH_MODE = process.env.AUTH_MODE === 'true'
+
+export {
+  PORT,
+  NODE_ENV,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+  AUTH_MODE,
+}
