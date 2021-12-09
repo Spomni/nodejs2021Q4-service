adfeb8ef679b3d8b4b9b920435600e60abbaad9d
spomni <mikhail.snitko@gmail.com>
2021-12-09 11:48:55 +0300

    feat: add error processing of the app.listen() method

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import { PORT } from './common/config'
 import * as app from './app'
 
-app.listen(PORT, () =>
+app.listen(PORT, (err: Error | null) => {
+
+  if (err) {
+   console.error(err)
+   process.exit(1)
+  }
+
   console.log(`App is running on http://localhost:${PORT}`)
-);
+});
