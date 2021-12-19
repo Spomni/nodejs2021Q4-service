892aa105db5c78a1f6f0690aed1683a085040ad7
spomni <mikhail.snitko@gmail.com>
2021-12-19 17:51:20 +0300

    docs: write TSDoc for the board.handlers module

--- src/resources/boards/board.handlers.ts
+++ src/resources/boards/board.handlers.ts
@@ -10,10 +10,23 @@ type RequestWithBoard = FastifyRequest<{
   Body: IBoard,
 }>
 
+/**
+ * Handle request to get all stored boards
+ *
+ * @returns all stored boards
+ */
 async function getAllBoards() {
   return boardService.getAll()
 }
 
+/**
+ * Handle request to get a board
+ *
+ * @param req - request to get board
+ * @param reply - fastify reply object
+ *
+ * @returns board or nothing if it is not found
+ */
 async function getBoard(req: RequestByBoardId, reply: FastifyReply) {
   const { boardId } = req.params
 
@@ -22,6 +35,14 @@ async function getBoard(req: RequestByBoardId, reply: FastifyReply) {
   return board || reply.callNotFound()
 }
 
+/**
+ * Handle request to create a new board
+ *
+ * @param req - request to create a board
+ * @param reply - fastify reply object
+ *
+ * @returns created board
+ */
 async function createBoard(req: RequestWithBoard, reply: FastifyReply) {
 
   const board = await boardService.create(req.body)
@@ -31,12 +52,24 @@ async function createBoard(req: RequestWithBoard, reply: FastifyReply) {
   return board
 }
 
+/**
+ * Handle request to remove a board
+ *
+ * @param req - request to remove a board
+ * @param reply - fastify reply object
+ */
 async function removeBoard(req: RequestByBoardId, reply: FastifyReply) {
   const { boardId } = req.params
   await boardService.removeById(boardId)
   reply.code(204)
 }
 
+/**
+ * Handle request to update a board
+ *
+ * @param req - request to remove a board
+ * @returns updated board
+ */
 async function updateBoard(req: RequestByBoardId & RequestWithBoard) {
   const { boardId } = req.params
   const board = await boardService.updateById(boardId, req.body)
