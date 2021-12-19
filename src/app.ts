26fbfab17fabb9093e21531ecb6836967b31d46e
spomni <mikhail.snitko@gmail.com>
2021-12-19 22:11:25 +0300

    docs: write TSDoc for the app module

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,25 @@ import { taskRouter } from './resources/tasks/task.router'
 import { NODE_ENV } from './common/config'
 import { RouteRegistrant, RouteConfig, PluginConfig } from './common/route-registrant'
 
+/**
+ * Check if the application is started in the development mode
+ *
+ * @returns checking result
+ */
 const isModeDev = () => NODE_ENV === 'development'
+
+/**
+ * Get a config to enable logging
+ *
+ * @returns fastify config to log warn pretty
+ */
 const logWarn = () => ({ logger: { level: 'warn', prettyPrint: true }})
+
+/**
+ * Get a config to disable logging
+ *
+ * @returns fastify config to disable logging
+ */
 const logNothing = () => ({ logger: false })
 
 const routeList = [
@@ -35,6 +52,11 @@ const routeList = [
   }
 ] as Array<RouteConfig | PluginConfig>
 
+/**
+ * Create a new application
+ *
+ * @returns application
+ */
 function createApp() {
 
   const app = Fastify({
